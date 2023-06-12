e84cb02422124a76d9520d40395a37f0c0206164
Rune <rune@vanderlei.be>
2023-06-12 06:55:17 +0200

    removed unused components

--- Minigin/ScoreCommand.cpp
+++
@@ -1,9 +0,0 @@
-#include "ScoreCommand.h"
-#include "Entity.h"
-#include "ScoreComponent.h"
-
-void ScoreCommand::Execute()
-{
-	auto scoreComponent = m_Entity->GetComponent<ScoreComponent>();
-	scoreComponent->AddScore(1);
-}
--- Minigin/ScoreCommand.h
+++
@@ -1,8 +0,0 @@
-#include "Command.h"
-
-class ScoreCommand final : public Command
-{
-public:
-	ScoreCommand(Entity* gobj) : Command{ gobj } {};
-	virtual void Execute() override;
-};
