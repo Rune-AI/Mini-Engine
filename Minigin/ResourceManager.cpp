3e0d00c6789a028d1bd464c55033ab003a1e468d
Rune <rune@vanderlei.be>
2023-06-13 23:56:13 +0200

    stored already loaded textures for any repeat calls

--- Minigin/ResourceManager.cpp
+++ Minigin/ResourceManager.cpp
@@ -17,15 +17,33 @@ void ResourceManager::Init(const std::string& dataPath)
 	}
 }
 
-std::shared_ptr<Texture2D> ResourceManager::LoadTexture(const std::string& file) const
+std::shared_ptr<Texture2D> ResourceManager::LoadTexture(const std::string& file)
 {
+	// unload any unused textures, we assume this function gets called when textures are being swapped, or the level is being loaded. So the poeration can take some time
+	// this function is not called every frame, so it's fine to do this here
+	UnloadUnusedTextures();
+
 	const auto fullPath = m_dataPath + file;
+
+	//check if texture is already loaded
+	auto it = m_LoadedTextures.find(fullPath);
+	if (it != m_LoadedTextures.end())
+	{
+		//if found, return it
+		return it->second;
+	}
+
+	// not found, load it
 	auto texture = IMG_LoadTexture(Renderer::GetInstance().GetSDLRenderer(), fullPath.c_str());
 	if (texture == nullptr)
 	{
 		throw std::runtime_error(std::string("Failed to load texture: ") + SDL_GetError());
 	}
-	return std::make_shared<Texture2D>(texture);
+
+	//add to loaded textures
+	m_LoadedTextures[fullPath] = std::make_shared<Texture2D>(texture);
+
+	return m_LoadedTextures[fullPath];
 }
 
 std::shared_ptr<Font> ResourceManager::LoadFont(const std::string& file, unsigned int size) const
@@ -37,4 +55,19 @@ std::string ResourceManager::LoadSound(const std::string& file) const
 {
 	const auto fullPath = m_dataPath + file;
 	return fullPath;
+}
+
+void ResourceManager::UnloadUnusedTextures()
+{
+	for (auto it = m_LoadedTextures.begin(); it != m_LoadedTextures.end();)
+	{
+		if (it->second.use_count() <= 1)
+		{
+			it = m_LoadedTextures.erase(it);
+		}
+		else
+		{
+			++it;
+		}
+	}
 }
--- Minigin/ResourceManager.h
+++ Minigin/ResourceManager.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <memory>
 #include "Singleton.h"
+#include <unordered_map>
 
 
 class Texture2D;
@@ -10,11 +11,15 @@ class ResourceManager final : public Singleton<ResourceManager>
 {
 public:
 	void Init(const std::string& data);
-	std::shared_ptr<Texture2D> LoadTexture(const std::string& file) const;
+	std::shared_ptr<Texture2D> LoadTexture(const std::string& file);
 	std::shared_ptr<Font> LoadFont(const std::string& file, unsigned int size) const;
 	std::string LoadSound(const std::string& file) const;
+
+	void UnloadUnusedTextures();
+
 private:
 	friend class Singleton<ResourceManager>;
 	ResourceManager() = default;
 	std::string m_dataPath;
+	std::unordered_map<std::string, std::shared_ptr<Texture2D>> m_LoadedTextures{};
 };
