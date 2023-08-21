7570bbcd0bd2600adc1876b61dab31a7e0faf6fc
Rune <rune@vanderlei.be>
2023-08-21 08:04:58 +0200

    added data to notices

--- Minigin/Subject.h
+++ Minigin/Subject.h
@@ -7,21 +7,21 @@
 namespace BearBones
 {
 
-	template<typename T>
+	template<typename T, typename D>
 	class Subject
 	{
 	public:
-		void RegisterObserver(IObserver<T>* observer)
+		void RegisterObserver(IObserver<T, D>* observer)
 		{
 			m_Observers.push_back(observer);
 		}
 
-		void RemoveObserver(IObserver<T>* observer)
+		void RemoveObserver(IObserver<T, D>* observer)
 		{
 			m_Observers.erase(std::remove(m_Observers.begin(), m_Observers.end(), observer), m_Observers.end());
 		}
 
-		void NotifyObservers(T data)
+		void NotifyObservers(D data)
 		{
 			for (auto observer : m_Observers)
 			{
@@ -29,8 +29,8 @@ namespace BearBones
 			}
 		}
 
-	private:
-		std::vector<IObserver<T>*> m_Observers;
+	protected:
+		std::vector<IObserver<T, D>*> m_Observers;
 	};
 }
 
